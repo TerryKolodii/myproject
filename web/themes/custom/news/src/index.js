356479624991cf2c4a97eda9f984ccfca45a2b8a
terry <t.kolodii@dev-branch.com>
2021-11-18 17:15:21 +0200

    task-3856 added scripts for tablinks

--- web/themes/custom/news/src/index.js
+++ web/themes/custom/news/src/index.js
@@ -11,6 +11,23 @@
       });
     }
   }
+})(Drupal, jQuery);
+
+
+(function (Drupal, $) {
+  Drupal.behaviors.changeNewslistByClick = {
+    attach: function (context, settings) {
+      $('.tab .tablinks:nth-child(2)').click(function () {
+        $('.block-views-blocknews-left-sidebar-block-1').slideUp(500);
+        $('.block-views-blocknews-left-sidebar-block-4').css('display', 'block');
+      });
+      $('.tab .tablinks:nth-child(1)').click(function () {
+        $('.block-views-blocknews-left-sidebar-block-4').slideUp(500);
+        $('.block-views-blocknews-left-sidebar-block-1').slideDown(500);
+
+      });
+    }
+  }
 })(Drupal, jQuery)
 
 
